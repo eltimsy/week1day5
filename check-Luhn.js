0b5cfdeb445ccbe418ce60482abb147ef58c15a6
eltimsy <timothy.sc.tse@gmail.com>
2016-09-08 23:18:50 +0000

    added luhn files

---
+++ check-Luhn.js
@@ -0,0 +1,58 @@
+'use strict';
+var newValues = [];
+
+function numberList(number){
+  var newNum = number;
+  var list = [];
+  for(var i = newNum; i > 0; i = Math.floor(i/10)){
+    list.push(newNum % 10);
+    newNum = Math.floor(newNum/10);
+  }
+  return list;
+}
+
+function editNum(num,index){
+  if (index%2 === 0){
+    var newNum = num * 2;
+
+    if (newNum > 9){
+      newNum -= 9;
+    };
+  }else{
+    var newNum = num;
+  }
+  newValues.push(newNum);
+}
+
+function checkLuhn(number){
+  newValues = [];
+  var lastNum = Number(number.toString().slice(-1));
+  var numMinusLast = Math.floor(number/10);
+  var total = 0
+  var numList = numberList(numMinusLast);
+
+  numList.forEach(editNum);
+
+  newValues.forEach(function(num){
+    total += num;
+  });
+
+  total += lastNum
+  console.log(newValues);
+  console.log(total);
+
+  if(total % 10 === 0){
+    return true;
+  } else {
+    return false;
+  };
+
+
+}
+
+
+
+console.log(checkLuhn(79927398710))
+console.log(checkLuhn(79927398711))
+console.log(checkLuhn(4941151854614372))
+console.log(checkLuhn(4748189605577626))
---
+++ test-check-Luhn.js
@@ -0,0 +1,2 @@
+var assert = require("chai").assert;
+var checkLuhn = require("../check-Luhn");
